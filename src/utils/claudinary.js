582fbca991633b14149797c4567c348266e5a6e7
ifeelsam <sanskarsharma9005@gmail.com>
2024-03-14 02:31:45 +0530

    updeted claudinary

--- src/utils/claudinary.js
+++ src/utils/claudinary.js
@@ -1,8 +1,31 @@
 import { v2 as cloudinary } from 'cloudinary';
-process.loadEnvFile();
+import fs from "fs";
 
 cloudinary.config({
   cloud_name: process.env.cloud_name,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
+
+const uploadCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resourse_type: "auto"
+    })
+    console.log("File uploaded!! ", response.url);
+    return response;
+  }
+  catch (error) {
+    fs.unlink(localFilePath)
+    return null;
+    // remove the locally saved file from the server!
+
+  }
+}
+
+
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function(error, result) { console.log(result); });
