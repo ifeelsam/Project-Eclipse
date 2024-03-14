253107040f13d58678f32cf33fba265633630d5f
ifeelsam <sanskarsharma9005@gmail.com>
2024-03-15 02:41:25 +0530

    multer updated

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, "./public/temp")
+  },
+  filename: function(req, file, callback) {
+
+    callback(null, file.originalname)
+  }
+})
+export const upload = multer({ storage: storage })
--- src/utils/claudinary.js
+++ src/utils/claudinary.js
@@ -29,3 +29,5 @@ const uploadCloudinary = async (localFilePath) => {
 cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
   { public_id: "olympic_flag" },
   function(error, result) { console.log(result); });
+
+export { uploadCloudinary }
